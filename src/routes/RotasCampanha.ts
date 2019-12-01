3445e8f14a1b66d429a84117d6a810cf6cd25394
Matheus Henrique <matheus.justino@ccc.ufcg.edu.br>
2019-12-01 12:17:23 -0300

    configs e tratamento de erros

--- src/routes/RotasCampanha.ts
+++ src/routes/RotasCampanha.ts
@@ -9,17 +9,37 @@ const router = express.Router();
 router.route("/campanhas")
     .get(authService, async (req: any, res: any): Promise<ICampanhaModel[]> => {
         const campanhas: ICampanhaModel[] = await apiCampanha.todasAsCampanhas();
-        return res.json(campanhas);
+        if (campanhas === null) {
+            return res.json("Nenhuma campanha cadastrada");
+        } else {
+            return res.json(campanhas);
+        }
     })
     .post(authService, async (req: any, res: any): Promise<ICampanhaModel> => {
-        const novaCampanha: ICampanhaModel = await apiCampanha.criarCampanha(req.body);
-        return res.json(novaCampanha);
+        try {
+            const novaCampanha: ICampanhaModel = await apiCampanha.criarCampanha(req.body);
+            return res.json(novaCampanha);
+        } catch (error) {
+            if (error.errmsg.includes("nomeCurto")) {
+                return res.json("nomeCurto já existente");
+            } else if (error.errmsg.includes("url")) {
+                return res.json("url já existente");
+            } else {
+                return res.json(error);
+            }
+        }
+        //const novaCampanha: ICampanhaModel = await apiCampanha.criarCampanha(req.body);
+        //return res.json(novaCampanha);
     })
 
 router.route("/campanha/:url")
     .get(authService, async (req: any, res: any): Promise<ICampanhaModel> => {
         const campanha: ICampanhaModel = await apiCampanha.campanhaByUrl(req.params.url);
-        return res.json(campanha);
+        if (campanha === null) {
+            return res.json("Campanha não encontrada")
+        } else {
+            return res.json(campanha);
+        }
     })
 
 
--- src/routes/RotasUsuarios.ts
+++ src/routes/RotasUsuarios.ts
@@ -9,17 +9,35 @@ const router = express.Router();
 router.route("/usuarios")
     .get(authService, async (req: any, res: any): Promise<IUsuarioModel[]> => {
         const usuarios: IUsuarioModel[] = await apiUsuario.todosOsUsuarios();
-        return res.json(usuarios);
+        if (usuarios === null) {
+            return res.status(404).json({ msg: "Nenhum usuário cadastrado" });
+        } else {
+            return res.json(usuarios);
+        }
     })
     .post(async (req: any, res: any): Promise<IUsuarioModel> => {
+        try {
         const novoUsuario: IUsuarioModel = await apiUsuario.criarUsuario(req.body);
-        return res.json(novoUsuario);
+        return res.json(novoUsuario);            
+        } catch (error) {
+            if (error.errmsg.includes("email")) {
+                return res.status(400).json({ msg: "Email já cadastrado" });
+            } else {
+                return res.json(error.errmsg);
+            }
+        }
+        //const novoUsuario: IUsuarioModel = await apiUsuario.criarUsuario(req.body);
+        //return res.json(novoUsuario);
     });
 
 router.route("/usuario")
     .get(authService, async (req: any, res: any): Promise<IUsuarioModel> => {
         const usuario: IUsuarioModel = await apiUsuario.usuarioPorEmail(req.body.email);
-        return res.json(usuario);
+        if (usuario === null) {
+            return res.status(404).json({ msg: "Usuário não encontrado" });
+        } else {
+            return res.json(usuario);
+        }
     });
 
 
