8212fdfa830552ab1db0aed98fc9b22c33442dcf
Matheus Henrique <matheus.justino@ccc.ufcg.edu.br>
2019-12-07 15:02:40 -0300

    apagando comentarios

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 require("dotenv-safe").config();
 import mongoose from "./database/ConexaoBD";
 import bodyParser from "body-parser";
+import { initRoutes } from "./Routes/router";
+import routes from "./Routes/routes";
+
 const express = require("express");
 const cors = require("cors");
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-//const rotasUsuario = require("./routes/RotasUsuarios");
-//const rotasLogin = require("./routes/RotasLogin");
-//const rotasCampanha = require("./routes/RotasCampanha");
 
-import { initRoutes } from "./Routes/router";
-import routes from "./Routes/routes";
+const router = initRoutes(routes);
 
 const PORT = 3333
 
@@ -23,9 +22,6 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
-//app.use("/api", [rotasLogin, rotasUsuario, rotasCampanha]);
-const router = initRoutes(routes);
-//console.log(router);
 app.use("/api", router);
 
 
